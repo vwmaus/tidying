add70d69c2c4688dc440d7243cd7547f0e4b26ce
acsoterroni <alinecsoterroni@gmail.com>
2017-05-09 11:07:51 +0200

    Added title.

--- acsoterroni/01-example.R
+++ acsoterroni/01-example.R
@@ -1,9 +1,11 @@
+#----------------------------------
 #First script
 #Aline Soterroni
 #08 may, 2017
-
+#----------------------------------
 x <- seq(0,10,0.01)
 y <- sin(x)
 z <- cos(x)
 plot(x,y,type="l",col="red")
 lines(x,z,type="l",col="green")
+title(main="sine and cosine waves")
