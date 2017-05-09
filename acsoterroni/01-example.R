33959596a21cc1f5d4b78400af03a3a6d833f569
acsoterroni <alinecsoterroni@gmail.com>
2017-05-09 11:14:34 +0200

    Changing color of sine wave.

--- acsoterroni/01-example.R
+++ acsoterroni/01-example.R
@@ -6,6 +6,6 @@
 x <- seq(0,10,0.01)
 y <- sin(x)
 z <- cos(x)
-plot(x,y,type="l",col="red",ylab="f(x)")
+plot(x,y,type="l",col="blue",ylab="f(x)")
 lines(x,z,type="l",col="green")
 title(main="sine and cosine waves")
